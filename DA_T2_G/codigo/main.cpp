94e262cd337420b8d4e9617762a1fc4ca46c4179
PedroCorreia <68613340+PedradasDaTuga@users.noreply.github.com>
2022-05-24 15:17:12 +0100

    Apenas cenas pra ler

--- DA_T2_G/codigo/main.cpp
+++ DA_T2_G/codigo/main.cpp
@@ -8,7 +8,37 @@
 #include "grafo.h"
 int main(){
 
+    string grafofile("../input/in01.txt");//Ficheiro com o grafo
+    string line;//string que guarda as linhas do ficheiro
+
+    ifstream input_file(grafofile);
+    //Verificar se a stream está associada a um ficheiro
+    if(!input_file.is_open()){
+        cout<<"Erro ao abrir o ficheiro "<<endl;
+    }
+
+    int temp=0;
+    //leitura do grafo
+    while(std::getline(input_file,line)){
+        char *dup = strdup(line.c_str());
+
+        if(temp==0){
+            int N=atoi(strtok(dup," "));//número de vértices
+            int R=atoi(strtok(NULL," "));//número de ramos
+            criar grafo
+            Grafo=///
+            temp++;
 
 
+        }
+        else{
+            int i = atoi(strtok(dup," "));
+            int j = atoi(strtok(NULL," "));
+            int capacidade = atoi(strtok(NULL," "));
+            int duracao= atoi(strtok(NULL," "));
+            free(dup);
+            guardar as merdas no grafo
+        }
+    }
 }
 
